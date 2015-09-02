5a2da4390db1f2465001255d51173bf7cd7da3a3
sunkay <sunkay11@gmail.com>
2015-09-01 22:27:06 -0400

    add firebase and reactfire bindAsObject integration to top level component

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-todo-firebase
 Todo react application using firebase, gulp, browserify, livereload, watchify, reactfire etc
+
+1. Clone repository
+2. npm install
+3. gulp
--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,21 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+
+var rootUrl = "https://radiant-torch-3052.firebaseio.com/";
 
 var Hello = React.createClass({
+  mixins: [ReactFire],
+
+  componentWillMount: function(){
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+  },
+
   render: function() {
+    console.log(this.state);
+    
     return <h1 className="red">
-      Hello!
+      Hello - ToDo!
     </h1>
   }
 });
